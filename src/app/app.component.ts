bf938ecf0403df84d9ac1d6681e25a2b92a04719
allanbrunobr <allanbruno@gmail.com>
2024-06-12 01:08:01 -0300

    Init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import { TodosComponent } from './todos/todos.component';
 import { Amplify } from 'aws-amplify';
 import outputs from '../../amplify_outputs.json';
-import { AmplifyAuthenticatorModule, AuthenticatorService } from '@aws-amplify/ui-angular';
+import {AmplifyAuthenticatorModule} from "@aws-amplify/ui-angular";
 
 Amplify.configure(outputs);
 
